9ec2efc964c5ffc322b95edfe671beb26865bebe
Nhinhoshxhy Desprez <noshdesprez@gmail.com>
2020-02-24 15:09:26 +0000

    first commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { Container, NavDropdown, Nav, Button, Form, FormControl } from "react-bootstrap";
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img
+					className="TPlogo"
+					src="https://files.slack.com/files-pri/T0BFXMWMV-FNJUVAP7D/tplogo.png"
+					alt="toppollLogo"
+				/>
+				<span className="navbar-brand mb-0 h1">
+					<strong>T</strong>
+					oppoll.net
+				</span>
 			</Link>
+			<Link to="/">
+				<Button variant="light">Home</Button>{" "}
+			</Link>
+			<Link to="/">
+				<Button variant="light">Disqus</Button>{" "}
+			</Link>
+
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<Link to="/">
+					<Button variant="light">Login</Button>{" "}
+				</Link>
+				<Link to="/">
+					<Button variant="light">Sign Up</Button>{" "}
 				</Link>
 			</div>
 		</nav>
