91a02fd0a73565df4b33ac9bfd1e053178da2724
Nhinhoshxhy Desprez <noshdesprez@gmail.com>
2020-02-25 17:29:08 +0000

    made the video function

---
+++ src/js/component/opener.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Welcome() {
+	return (
+		<div className="greetings" style={{ position: "absolute", width: "100%" }}>
+			<div className="head-greetings" style={{ "text-align": "center", "padding-top": 200 }}>
+				<h1>
+					Welcome to <strong>T</strong>
+					oppoll
+				</h1>
+				<h2>The bowl to flood your thoughts into!</h2>
+				<h3>Express yourself!!</h3>
+			</div>
+		</div>
+	);
+}
