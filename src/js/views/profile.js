abaac025cbf5d33611f11b8fc9bd84d56af2488a
Nhinhoshxhy Desprez <noshdesprez@gmail.com>
2020-03-12 19:30:43 +0000

    update

--- src/js/views/profile.js
+++ src/js/views/profile.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Welcome, Take_space, TakeSpace } from "../component/opener.js";
+import { ToppollCard } from "../component/toppollCard.js";
 import { Container, NavDropdown, Nav, Button, Form, FormControl, Jumbotron } from "react-bootstrap";
 import ScrollAnimation from "react-animate-on-scroll";
 import ScrollUpButton from "react-scroll-up-button";
-import { ParallaxProvider, Parallax } from "react-scroll-parallax";
+import { ToppollCarousel } from "../component/toppollCarousel.js";
+import { Context } from "../store/appContext";
 import { Footer } from "../component/footer.js";
 import { Link } from "react-router-dom";
 
@@ -11,13 +13,24 @@ export const Profile = () => {
 	return (
 		<div className="profilePage">
 			<div className="container profilePicBack">
-				<img
-					src="https://www.freeiconspng.com/uploads/account-profile-icon-2.png"
-					style={{ width: "100%", height: "400px" }}
-				/>
-			</div>
-			<div className="container profilePic">
 				<img src="https://www.freeiconspng.com/uploads/account-profile-icon-2.png" />
+			</div>
+			<div className="container profilePic" style={{ position: "relative", bottom: "10%", left: "2%" }}>
+				<img src="https://www.freeiconspng.com/uploads/account-profile-icon-2.png" />
+				<h1 className="mt-3">Username</h1>
+				<h5>Date</h5>
+			</div>
+			<div className="container favPollsHead">
+				<h3>Polls you saved</h3>
+			</div>
+			<div className="container favPolls">
+				<TakeSpace />
+				<TakeSpace />
+				<TakeSpace />
+				<TakeSpace />
+				<TakeSpace />
+				<TakeSpace />
+				<div className="" />
 			</div>
 
 			<Footer />
